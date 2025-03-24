2b7541abb2bf03d32fb362aa19d585faf65e3907
VedantMulherkar <vmulherkar@xtsworld.in>
2025-03-24 19:50:41 +0530

    worked on sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 import Layout from "./component/Layout";
 import Home from "./pages/Home";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function App() {
   return (
--- frontend/src/component/Sidebar.js
+++ frontend/src/component/Sidebar.js
@@ -1,15 +1,24 @@
 import React from "react";
-import { Nav } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../css/sidebar.css";
 
 const Sidebar = () => {
   return (
-    <div className="bg-dark text-white p-3 vh-100 position-fixed" style={{ width: "250px" }}>
-      <h4 className="text-center">Sidebar</h4>
-      <Nav className="flex-column">
-        <Nav.Link href="/" className="text-white">Home</Nav.Link>
-        <Nav.Link href="/about" className="text-white">About</Nav.Link>
-        <Nav.Link href="/contact" className="text-white">Contact</Nav.Link>
-      </Nav>
+    <div id="layoutSidenav">
+      <div id="layoutSidenav_nav">
+        <nav className="sb-sidenav accordion sb-sidenav-dark" role="navigation" id="sidenavAccordion">
+          <div className="sb-sidenav-menu pt-4 bg-dark">
+            <div className="nav">
+              <button type="button" className="nav-link remove-btn-border" data-toggle="tab" id="dashboardBtn">
+                <div className="sb-nav-link-icon">
+                  <i className="fas fa-tachometer-alt" aria-hidden="true"></i>
+                </div>
+                Dashboard
+              </button>
+            </div>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 };
